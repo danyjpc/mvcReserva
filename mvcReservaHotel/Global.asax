6115cfacbcd1b1f889683b9070b73dc7f6aecb66
danyjpc <yjoedany@gmail.com>
2017-11-05 14:45:00 -0600

    Version inicial

---
+++ mvcReservaHotel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcReservaHotel.MvcApplication" Language="C#" %>
---
+++ mvcReservaHotel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcReservaHotel.Startup))]
+namespace mvcReservaHotel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
